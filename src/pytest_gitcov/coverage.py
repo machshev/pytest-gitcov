870ef918db7c3799f4ebdf116bdfebc426733dbd
David James McCorrie <djmccorrie@gmail.com>
2020-01-06 16:26:00 +0000

    coverage file timestamp and generator for covered files

--- src/pytest_gitcov/coverage.py
+++ src/pytest_gitcov/coverage.py
@@ -1,5 +1,8 @@
 """Coverage parsing
 """
+import os
+
+from typing import Iterable
 
 from coverage import CoverageData
 
@@ -20,8 +23,36 @@ def get_coverage_from_file(path: str = '.coverage') -> CoverageData:
         CoverageData: previously recorded test coverage
     """
     cov_data = CoverageData(basename=path)
-    #cov_data.read()
 
     assert cov_data, 'CoverageData is loaded'
 
     return cov_data
+
+
+def get_coverage_timestamp(cov_data: CoverageData) -> float:
+    """Get the time modified for the coverage data
+
+    Args:
+        cov_data (CoverageData): coverage data object
+
+    Returns:
+        float: time stamp of the last modification of coverage file
+    """
+    path = cov_data.base_filename()
+    timestamp = os.path.getmtime(path)
+
+    return timestamp
+
+
+def files_covered(cov_data: CoverageData) -> Iterable[str]:
+    """Generate a list of file
+
+    Args:
+        cov_data (CoverageData): coverage data object
+
+    Yields:
+        str: path for each of the files with coverage by tests
+    """
+    for path in cov_data.measured_files():
+        if path.endswith('.py'):
+            yield path
